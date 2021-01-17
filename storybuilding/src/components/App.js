65c9b0920bc163e6612ebab3e8255e51b202e331
Adelaide Evans <60193245+00ahevans@users.noreply.github.com>
2021-01-17 00:09:28 -0500

    barebones authentication

--- storybuilding/src/components/App.js
+++ storybuilding/src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Signup from "./Signup";
+import Login from "./Login"
+import Home from "./Home"
 import {AuthProvider} from "../contexts/AuthContext.js"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
@@ -7,7 +9,14 @@ function App() {
   return (
   <Router>
   <AuthProvider>
-    <Signup />
+        <div>
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/signup" component={Signup}/>
+             <Route path="/login" component={Login}/>
+            <Route component={Error}/>
+           </Switch>
+        </div> 
   </AuthProvider>
   </Router>
   )
